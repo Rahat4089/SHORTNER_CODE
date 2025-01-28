01d07eebb99c5582519f6fb377568db6f732ad7b
Rahat4089 <83128928+Rahat4089@users.noreply.github.com>
2025-01-28 12:42:57 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
-import os
 
-API_ID = 24720817  # Replace with your API ID
-API_HASH = "43669876f7dbd754e157c69c89ebf3eb"  # Replace with your API hash
-BOT_TOKEN = "7927514116:AAEJU5UoE4rMFTWj8pA8V7ENVogU91wWRCY"
-DB_URI = "mongodb+srv://094:094@cluster0.mixuhkk.mongodb.net/?retryWrites=true&w=majority"
-DB_NAME = "shortlink"
+DB_URI = "" # Add MongoDB URL Here
+DB_NAME = "" # Add MongoDB Name Here
 
 
 
@@ -17,5 +13,5 @@ You Can Use Me Now .
 IS_VERIFY = True  # Enable or disable verification
 VERIFY_EXPIRE = 600 # Token expiry time in seconds (1 hours)
 SHORTLINK_URL = "instantearn.in"  # Replace with your shortlink URL
-SHORTLINK_API = "f1387b1312d362e2698861160b0dd1f78d14b12c"
+SHORTLINK_API = "f1387b1312d362e2698861160b0dd1f78d14b12c"  # Replace with your shortlink API
 TUT_VID = "https://t.me/r4h4t_69"  # Tutorial video link
